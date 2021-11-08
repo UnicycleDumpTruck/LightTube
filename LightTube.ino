fc410bc18eefa7f14ca65725dd2740f9b793fda0
unicycledumptruck <junk@jeffhighsmith.com>
2021-11-08 12:44:05 -0500

    Added RFM69 radio code

--- LightTube.ino
+++ LightTube.ino
@@ -2,16 +2,91 @@
 #include <SPI.h>
 #include <Adafruit_VS1053.h>
 #include <Adafruit_DotStar.h>
+#include <Adafruit_SleepyDog.h>
+
+
+// Radio Includes and variables ----------------------------------
+
+#include <RH_RF69.h>
+#include <RHReliableDatagram.h>
+
+#define RF69_FREQ 915.0
+
+// Where to send packets to!
+//#define DEST_ADDRESS   1
+// change addresses for each client board, any number :)
+#define MY_ADDRESS     90
+
+// Feather M0 w/Radio
+#define RFM69_CS      A4
+#define RFM69_INT     A3
+#define RFM69_RST     11
+
+// Singleton instance of the radio driver
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram rf69_manager(rf69, MY_ADDRESS);
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+uint32_t localCounter = 0;
+
+struct EvenDataPacket{
+  uint32_t counter;
+  float batteryVoltage;
+  uint8_t cubeID;
+  uint8_t side;
+} eventData;
+
+// Dont put this on the stack:
+uint8_t eventBuffer[sizeof(eventData)];
+uint8_t from;
+uint8_t len = sizeof(eventData);
+
+
+void selectRadio() {
+  //digitalWrite(LED,HIGH);
+  //digitalWrite(WIZ_CS, HIGH);
+  //delay(100);
+  digitalWrite(RFM69_CS, LOW);
+  //delay(100);
+}
+
+
+//RF communication
+void sendEventData()
+{  
+	selectRadio();
+  rf69.send((uint8_t*)&eventData, sizeof(eventData));
+	rf69.waitPacketSent();
+}
+
+void sendGoEvent()
+{
+  eventData.side = queueTail;
+  eventData.cubeID = MY_ADDRESS;
+  eventData.batteryVoltage = 0;
+  eventData.counter++;
+  Serial.print("About to send transmission number: ");
+  Serial.println(eventData.counter);      
+  sendEventData();
+  //eventData.side = 0;
+}
+
+
+
 
 #define SHIELD_RESET  -1    // VS1053 reset pin (unused!)
 #define SHIELD_CS     6     // VS1053 chip select pin (output)
 #define SHIELD_DCS    10    // VS1053 Data/command select pin (output)
-#define CARDCS 5            // Card chip select pin
-#define DREQ 9              // VS1053 Data request, ideally an Interrupt pin
+#define CARDCS        5     // Card chip select pin
+#define DREQ          9     // VS1053 Data request, ideally an Interrupt pin
 
+// DotStar Strip Size
 #define NUMPIXELS 158 // 1 has 158, 2 has 139 // Number of LEDs in strip
 
-// Here's how to control the LEDs from any two pins:
+// DotStar Control
 #define DATAPIN A2 // Station 2 is A1, Station 1 is A2
 #define CLOCKPIN A1 // Station 2 is A2, Station 1 is A1
 Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
@@ -121,6 +196,57 @@ void setup()
   // Set volume for left, right channels. lower numbers == louder volume!
   musicPlayer.setVolume(0,0);
   musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+
+  	//--Radio Setup--//
+	selectRadio();
+	//pinMode(LED, OUTPUT);     
+	pinMode(RFM69_RST, OUTPUT);
+	digitalWrite(RFM69_RST, LOW);
+
+	Serial.println("Feather Addressed RFM69 TX Test!");
+	Serial.println();
+
+	// manual reset
+	digitalWrite(RFM69_RST, HIGH);
+	delay(50);
+	digitalWrite(RFM69_RST, LOW);
+	delay(50);
+  
+	if (!rf69_manager.init()) {
+		Serial.println("RFM69 radio init failed");
+		while (1);
+	}
+	Serial.println("RFM69 radio init OK!");
+	// Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+	// No encryption
+	if (!rf69.setFrequency(RF69_FREQ)) {
+		Serial.println("setFrequency failed");
+	}
+
+	// If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+	// ishighpowermodule flag set like this:
+	rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+
+	// The encryption key has to be the same as the one in the server
+	uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+					0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+	rf69.setEncryptionKey(key);  
+
+	eventData.cubeID = 80; // 80 is the Marble Maze
+	eventData.side = 0;
+	eventData.batteryVoltage = 0;
+	eventData.counter = 0;
+
+  // CALIBRATION LOOP
+  /*for(int cal=0; cal<4; cal++) {
+    upAll();
+    delay(3000);
+    restAll();
+    delay(3000);
+  }*/
+
+	Watchdog.enable(4000);
+
 }
 
 
@@ -138,4 +264,5 @@ void loop()
         colorWipe(strip.Color(0,0,0), 5);
         musicPlayer.stopPlaying();
     }
+    Watchdog.reset();
 }
