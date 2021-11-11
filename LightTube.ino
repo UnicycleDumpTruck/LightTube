70d0dc94b32a1e48595dc4f2c6f70beb3d4a3720
unicycledumptruck <junk@jeffhighsmith.com>
2021-11-11 15:13:08 -0500

    Light and sound working, radio not

--- LightTube.ino
+++ LightTube.ino
@@ -15,12 +15,13 @@
 // Where to send packets to!
 //#define DEST_ADDRESS   1
 // change addresses for each client board, any number :)
-#define MY_ADDRESS     85 // Station 1 is 85, Station 2 is 86
+#define MY_ADDRESS     86 // Station 1 is 85, Station 2 is 86
 
 // Feather M0 w/Radio
 #define RFM69_CS      A4
-#define RFM69_INT     A3
+#define RFM69_INT     A5
 #define RFM69_RST     11
+#define RFM69_EN      A3
 
 // Singleton instance of the radio driver
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
@@ -57,9 +58,10 @@ void selectRadio() {
 //RF communication
 void sendEventData()
 {  
-	selectRadio();
+  digitalWrite(RFM69_CS, LOW);
   rf69.send((uint8_t*)&eventData, sizeof(eventData));
 	rf69.waitPacketSent();
+  digitalWrite(RFM69_CS, HIGH);
 }
 
 void sendGoEvent(uint8_t s)
@@ -84,11 +86,11 @@ void sendGoEvent(uint8_t s)
 #define DREQ          9     // VS1053 Data request, ideally an Interrupt pin
 
 // DotStar Strip Size
-#define NUMPIXELS 158 // 1 has 158, 2 has 139 // Number of LEDs in strip
+#define NUMPIXELS 139 // 1 has 158, 2 has 139 // Number of LEDs in strip
 
 // DotStar Control
-#define DATAPIN A2 // Station 2 is A1, Station 1 is A2
-#define CLOCKPIN A1 // Station 2 is A2, Station 1 is A1
+#define DATAPIN A1 // Station 2 is A1, Station 1 is A2
+#define CLOCKPIN A2 // Station 2 is A2, Station 1 is A1
 Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
 
 // Audio Sense Variables
@@ -97,31 +99,31 @@ Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
 Adafruit_VS1053_FilePlayer musicPlayer = 
   Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
 
-/// File listing helper
-void printDirectory(File dir, int numTabs) {
-   while(true) {
+// /// File listing helper
+// void printDirectory(File dir, int numTabs) {
+//    while(true) {
      
-     File entry =  dir.openNextFile();
-     if (! entry) {
-       // no more files
-       //Serial.println("**nomorefiles**");
-       break;
-     }
-     for (uint8_t i=0; i<numTabs; i++) {
-       Serial.print('\t');
-     }
-     Serial.print(entry.name());
-     if (entry.isDirectory()) {
-       Serial.println("/");
-       printDirectory(entry, numTabs+1);
-     } else {
-       // files have sizes, directories do not
-       Serial.print("\t\t");
-       Serial.println(entry.size(), DEC);
-     }
-     entry.close();
-   }
-}
+//      File entry =  dir.openNextFile();
+//      if (! entry) {
+//        // no more files
+//        //Serial.println("**nomorefiles**");
+//        break;
+//      }
+//      for (uint8_t i=0; i<numTabs; i++) {
+//        Serial.print('\t');
+//      }
+//      Serial.print(entry.name());
+//      if (entry.isDirectory()) {
+//        Serial.println("/");
+//        printDirectory(entry, numTabs+1);
+//      } else {
+//        // files have sizes, directories do not
+//        Serial.print("\t\t");
+//        Serial.println(entry.size(), DEC);
+//      }
+//      entry.close();
+//    }
+// }
 
 
 // Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
@@ -176,17 +178,75 @@ void setup()
   strip.show();  // Turn all LEDs off ASAP
   
   pinMode(AUDIO_SENSE_PIN, INPUT);
+  // pinMode(CARDCS, OUTPUT);
+  // pinMode(SHIELD_CS, OUTPUT);
+  //pinMode(RFM69_CS, OUTPUT); // This line caused the audio shield to not be found?
 
   Serial.begin(9600);
   // while (!Serial) {
   //     ; // wait for serial port to connect. Needed for native USB port only
   // }
   Serial.println("LightTube, Adafruit VS1053, RFM69");
+  // digitalWrite(SHIELD_CS, LOW);
+  
 
+
+  
+  
+  //   	//--Radio Setup--//
+
+  // digitalWrite(RFM69_CS, LOW);
+  // pinMode(RFM69_EN, OUTPUT);
+  // digitalWrite(RFM69_EN, HIGH);
+	// selectRadio();
+	// pinMode(RFM69_RST, OUTPUT);
+	// digitalWrite(RFM69_RST, LOW);
+
+	// // manual reset
+	// digitalWrite(RFM69_RST, HIGH);
+	// delay(50);
+	// digitalWrite(RFM69_RST, LOW);
+	// delay(50);
+  
+	// if (!rf69_manager.init()) {
+	// 	Serial.println("RFM69 radio init failed");
+	// 	while (1);
+	// }
+	// Serial.println("RFM69 radio init OK!");
+	// // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+	// // No encryption
+	// if (!rf69.setFrequency(RF69_FREQ)) {
+	// 	Serial.println("setFrequency failed");
+	// }
+
+	// // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+	// // ishighpowermodule flag set like this:
+	// rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+
+	// // The encryption key has to be the same as the one in the server
+	// uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+	// 				0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+	// rf69.setEncryptionKey(key);  
+  // digitalWrite(RFM69_CS, HIGH);
+
+
+	// eventData.cubeID = MY_ADDRESS;
+	// eventData.side = 0;
+	// eventData.batteryVoltage = 0;
+	// eventData.counter = 0;
+
+  // //sendGoEvent(0);
+
+  
+  
+  
+  
+  delay(100);
   if (! musicPlayer.begin()) { // initialise the music player
       Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
       while (1);
   }
+  //digitalWrite(SHIELD_CS, HIGH);
   Serial.println(F("VS1053 found"));
   
   if (!SD.begin(CARDCS)) {
@@ -195,50 +255,11 @@ void setup()
   }
   
   // Set volume for left, right channels. lower numbers == louder volume!
+  digitalWrite(SHIELD_CS, LOW);
   musicPlayer.setVolume(0,0);
   musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+  digitalWrite(SHIELD_CS, HIGH);
 
-  	//--Radio Setup--//
-	selectRadio();
-	//pinMode(LED, OUTPUT);     
-	pinMode(RFM69_RST, OUTPUT);
-	digitalWrite(RFM69_RST, LOW);
-
-	Serial.println("Feather Addressed RFM69 TX Test!");
-	Serial.println();
-
-	// manual reset
-	digitalWrite(RFM69_RST, HIGH);
-	delay(50);
-	digitalWrite(RFM69_RST, LOW);
-	delay(50);
-  
-	if (!rf69_manager.init()) {
-		Serial.println("RFM69 radio init failed");
-		while (1);
-	}
-	Serial.println("RFM69 radio init OK!");
-	// Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
-	// No encryption
-	if (!rf69.setFrequency(RF69_FREQ)) {
-		Serial.println("setFrequency failed");
-	}
-
-	// If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
-	// ishighpowermodule flag set like this:
-	rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
-
-	// The encryption key has to be the same as the one in the server
-	uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-					0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-	rf69.setEncryptionKey(key);  
-
-	eventData.cubeID = 80; // 80 is the Marble Maze
-	eventData.side = 0;
-	eventData.batteryVoltage = 0;
-	eventData.counter = 0;
-
-  sendGoEvent(0);
 
 	Watchdog.enable(4000);
   Serial.println("Setup Complete");
@@ -248,17 +269,21 @@ void setup()
 void loop()
 {
     int audio_reading = analogRead(AUDIO_SENSE_PIN);
-    // Serial.println(audio_reading);
+    //Serial.println(audio_reading);
     // delay(10);
     if (audio_reading > 22)
     {
         Serial.println(F("Playing Sound"));
         //colorWipe(GREEN, 5);
+        digitalWrite(SHIELD_CS, LOW);
         musicPlayer.startPlayingFile("/track002.mp3");
+        digitalWrite(SHIELD_CS, HIGH);
         rainbow(1);
-        sendGoEvent(1);
+        //sendGoEvent(1);
         colorWipe(strip.Color(0,0,0), 5);
+        digitalWrite(SHIELD_CS, LOW);
         musicPlayer.stopPlaying();
+        digitalWrite(SHIELD_CS, HIGH);
     }
     Watchdog.reset();
 }
